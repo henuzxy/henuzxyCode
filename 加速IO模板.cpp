63e835a51699c587baf7373284d1f94534f3fc12
henuzxy <30334058+zhao5502169@users.noreply.github.com>
2018-08-16 17:09:31 +0800

    IO模板

---
+++ 加速IO模板.cpp
@@ -0,0 +1,42 @@
+struct FastIO {
+    static const int S = 2 * maxlen;
+    int wpos;
+    char wbuf[S];
+    FastIO() : wpos(0) {}
+    inline int xchar() {
+        static char buf[S];
+        static int len = 0, pos = 0;
+        if (pos == len) pos = 0, len = fread(buf, 1, S, stdin);
+        if (pos == len) exit(0);
+        return buf[pos++];
+    }
+    inline ll xint() {
+        int s = 1, c = xchar();
+        ll x = 0;
+        while (c <= 32) c = xchar();
+        if (c == '-') s = -1, c = xchar();
+        for (; '0' <= c && c <= '9'; c = xchar()) x = x * 10 + c - '0';
+        return x * s;
+    }
+    inline void xstring(char *s) {
+        int c = xchar();
+        while (c <= 32) c = xchar();
+        for (; c > 32; c = xchar()) *s++ = c;
+        *s = 0;
+    }
+    inline void wchar(int x) {
+        if (wpos == S) fwrite(wbuf, 1, S, stdout), wpos = 0;
+        wbuf[wpos++] = x;
+    }
+    inline void wint(ll x) {
+        if (x < 0) wchar('-'), x = -x;
+        char s[24];
+        int n = 0;
+        while (x || !n) s[n++] = '0' + x % 10, x /= 10;
+        while (n--) wchar(s[n]);
+    }
+ 
+    ~FastIO() {
+        if (wpos) fwrite(wbuf, 1, wpos, stdout), wpos = 0;
+    }
+} io;
