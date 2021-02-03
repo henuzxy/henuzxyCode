39ef554beb789b6abe670edd4e657e17e74079d6
henuzxy <1961714414@qq.com>
2021-02-04 00:51:29 +0800

    update: 阶乘逆元

--- 数论/阶乘的逆元.cpp
+++ 数论/阶乘的逆元.cpp
@@ -5,19 +5,30 @@ typedef long long ll;
 const ll Mod = 998244353;
 const int MAX = 1e6+10;
 ll fac_inv[MAX],fac[MAX];
-ll Quick_pow(ll x,ll N){
+ll quick_pow(ll X,ll Y){
     ll res = 1;
-    while(N > 0){
-        if(N&1) res = res*x%Mod;
-        x = x%Mod*x%Mod;
-        N >>= 1;
+    while(Y > 0){
+        if(Y&1) res = res*X%Mod;
+        X = X%Mod*X%Mod;
+        Y >>= 1;
     }
     return res;
 }
-void Init_inv(){
-    int N = 1e6+3;
+void Init_inv(int N){
     fac[0] = fac[1] = 1;
-    for(int i=2;i<=N;++i)   fac[i] = fac[i-1]*i%Mod;
-    fac_inv[N] = Quick_pow(fac[N],Mod-2);
+    for(int i=2;i<=N;++i){
+        fac[i] = fac[i-1]*i%Mod;
+    }   
+    fac_inv[N] = quick_pow(fac[N],Mod-2);
     for(int i=N-1;i>=0;--i) fac_inv[i] = fac_inv[i+1]*(i+1)%Mod;
+}
+
+
+//更加容易写出的方法
+void Init_inv(int N){
+    fac[0] = fac_inv[0] = 1;
+    for(int i=1;i<=N;++i){
+        fac[i] = fac[i-1]*i%Mod;
+        fac_inv[i] = quick_pow(fac[i],Mod-2);
+    }
 }
